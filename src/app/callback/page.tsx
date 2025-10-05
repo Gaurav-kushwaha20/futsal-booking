e0f05cbf2201e2f49ed11511789c3e024b1aa512
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-10-05 20:04:57 +0545

    login and signup using keycloak

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -3,13 +3,21 @@
 import { useEffect } from "react";
 import { userManager } from "@/lib/oidc-client";
 import { useRouter } from "next/navigation";
+import { usePostDataMutation } from "@/service/api";
+import { apiTags, endpoints } from "@/constant/endpoints.constant";
 
 const Page = () => {
+    const [createUser, { isLoading }] = usePostDataMutation()
     const router = useRouter();
     useEffect(() => {
-        userManager.signinRedirectCallback().then(user => {
-            console.log("Logged in user:", user);
-            router.push("/");
+        userManager.signinRedirectCallback().then(async user => {
+            const response = await createUser({
+                url: endpoints.auth.createUser,
+                data: { keycloakId: user.profile.sub },
+                invalidateTag: [apiTags.createUser]
+            })
+            console.log(user);
+            router.replace("/",);
         });
     }, [router]);
 
--- src/constant/endpoints.constant.ts
+++ src/constant/endpoints.constant.ts
@@ -1,35 +1,37 @@
 export const endpoints = {
- auth: {
-  refreshtoken: '/auth/refresh',
-  me: '/auth/me',
- },
- userLogin: '/auth/login',
- ownerLogin: '/auth/owner/login',
- userRegister: '/auth/user/register',
- getAllFutsal: '/futsal/get-all',
- owner: {
-  futsalList: '/futsal/owner/get',
-  createFutsals: '/futsal/register',
-  getFutsalsDetails: 'futsal/get/',
-  updateFutsal: '/futsal/update/',
-  deleteFutsal: '/futsal/delete/',
- },
- timeSlot: {
-  create: '/time-slot/create/id',
-  list: '/time-slot/get-all/futsalid',
-  details: '/time-slot/details/id',
-  update: '/time-slot/update/id',
-  delete: 'time-slot/delete/id',
-  getByFutsalId: '/time-slot/get-all/id',
- },
- booking: {
-  create: '/booking/create?futsal=id',
-  getUserBooking: '/booking/user',
-  getOwnerBooking: '/booking/owner',
- },
+	auth: {
+		createUser: '/public/auth/user/register',
+		refreshtoken: '/auth/refresh',
+		me: '/auth/me',
+	},
+	userLogin: '/auth/login',
+	ownerLogin: '/auth/owner/login',
+	userRegister: '/auth/user/register',
+	getAllFutsal: '/futsal/get-all',
+	owner: {
+		futsalList: '/futsal/owner/get',
+		createFutsals: '/futsal/register',
+		getFutsalsDetails: 'futsal/get/',
+		updateFutsal: '/futsal/update/',
+		deleteFutsal: '/futsal/delete/',
+	},
+	timeSlot: {
+		create: '/time-slot/create/id',
+		list: '/time-slot/get-all/futsalid',
+		details: '/time-slot/details/id',
+		update: '/time-slot/update/id',
+		delete: 'time-slot/delete/id',
+		getByFutsalId: '/time-slot/get-all/id',
+	},
+	booking: {
+		create: '/booking/create?futsal=id',
+		getUserBooking: '/booking/user',
+		getOwnerBooking: '/booking/owner',
+	},
 };
 
 export const apiTags = {
- ownerGetAllFutsals: 'owner_get_all_futsals',
- get_owner_time_slot: 'get_owner_time_slot',
+	createUser: 'create_new_user',
+	ownerGetAllFutsals: 'owner_get_all_futsals',
+	get_owner_time_slot: 'get_owner_time_slot',
 };
