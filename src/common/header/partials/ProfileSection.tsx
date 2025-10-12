c8395a34a725cef596967e40c84807583581d9a6
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-10-13 00:54:12 +0545

    user logged in as google

--- src/common/header/partials/ProfileSection.tsx
+++ src/common/header/partials/ProfileSection.tsx
@@ -2,15 +2,15 @@
 
 import { useClickOutside } from "@/lib/useClickOutside";
 import { RootState } from "@/store/store";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { IoSettingsOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
 
 const ProfileSection = () => {
   const profileMenu = useClickOutside()
   const profile = useSelector((state: RootState) => state.auth.user?.profilePicture)
-  const { data: session } = useSession();
-
+  const { data } = useSession();
+  console.log(data)
   const handleSignInUser = () => {
     signIn("keycloak")
   }
@@ -19,14 +19,16 @@ const ProfileSection = () => {
 
   }
   const handleLogout = () => {
-
+    signOut({
+      callbackUrl: `http://localhost:8080/realms/futsal-realm/protocol/openid-connect/logout`
+    })
   }
   return (
     <div className="flex items-center gap-1 bg-primary-100 p-1 rounded-full w-fit">
       {/* Profile Avatar */}
       <div className="rounded-full w-12 h-10 overflow-hidden">
         <img
-          src={profile || '/profile.png'}
+          src={data?.user?.image || '/profile.png'}
           alt="Profile"
           className="w-full h-full object-cover hover-scale-125"
         />
