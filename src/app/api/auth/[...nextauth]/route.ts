7164bce70af98cd8d586b3eff98794885d74155c
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-10-16 23:54:39 +0545

    login with google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -16,17 +16,50 @@ export const authOptions: NextAuthOptions = {
 		}),
 	],
 	callbacks: {
-		async jwt({ token }) {
+		async jwt({ token, account }) {
 			console.log('token-------->', token);
+
+			// first login
+			if (account) {
+				token.access_token = account.access_token;
+				token.refresh_token = account.refresh_token;
+				token.id_token = account.id_token;
+				token.expires_at = Number(Math.floor(Date.now() / 1000 + account?.expires_at!));
+			}
+
+			// refresh if expired
+			if (Date.now() / 1000 > token?.expires_at) {
+				try {
+					const response = await fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+						body: new URLSearchParams({
+							client_id: process.env.KEYCLOAK_CLIENT_ID!,
+							client_secret: process.env.KEYCLOAK_CLIENT_SECRET!,
+							grant_type: 'refresh_token',
+							refresh_token: token.refresh_token as string,
+						}),
+					});
+					const refreshed = await response.json();
+
+					token.access_token = refreshed.access_token;
+					token.refresh_token = refreshed.refresh_token ?? token.refresh_token;
+					token.expires_at = Math.floor(Date.now() / 1000 + refreshed.expires_in);
+				} catch (err) {
+					console.error('Error refreshing Keycloak token', err);
+					return null; // user must re-login
+				}
+			}
 			return token;
 		},
 		async session({ session, token }) {
-			console.log('image--->', token?.picture);
 			session.user = {
-				name: token?.name,
-				email: token?.email,
-				image: token?.picture,
+				...session.user,
+				access_token: token.access_token,
+				refresh_token: token.refresh_token,
+				id_token: token.id_token,
 			};
+			return session;
 			return session;
 		},
 	},
