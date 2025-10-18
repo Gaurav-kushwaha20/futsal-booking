40e73519fc3dd862b8b2a6cbc3de2d3648cc7e07
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-10-18 21:52:38 +0545

    move provider to client component

---
+++ src/components/providers/Provider.tsx
@@ -0,0 +1,16 @@
+// components/Providers.tsx
+"use client";
+
+import { ReduxProvider } from "@/components/ReduxProvider";
+import ProtectedRoutes from "@/components/ProtectedRoutes";
+import { SessionProvider } from "next-auth/react";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <ReduxProvider>
+            <SessionProvider>
+                <ProtectedRoutes>{children}</ProtectedRoutes>
+            </SessionProvider>
+        </ReduxProvider>
+    );
+}
